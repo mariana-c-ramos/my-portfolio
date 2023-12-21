60f7f30537cfaa0e858e602309a5b3ac53a1788b
Mariana Ramos <mary.programms@gmail.com>
2023-12-21 20:13:57 +0000

    create about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,8 @@
+import styles from './about.module.css';
+import { getImageURL } from "../../utils";
+
+export const About = () => {
+  return (
+    <div className={styles.test}>About</div>
+  )
+}
