7eb11ccaa68fe07e9a758a9bcdc6939abc32702a
Mariana Ramos <mary.programms@gmail.com>
2024-01-02 13:31:04 +0000

    update projects and fonts

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,11 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I&apos;m Mariana</h1>
-        <p className={styles.description}>I&apos;m a frontend developer with almost 2y of experience using JavaScript and jQuery. Reach out if you would like to know more.</p>
-        <a className={styles.contactBtn} href="mailto:mary.programms@gmail.com">Contact me</a>
+        <p className={styles.description}>I&apos;m a Frontend Developer and I have been creating <strong className={styles.span}>lading pages</strong> and <strong className={styles.span}>gamified HTML5</strong> solutions. I work with JavaScript and jQuery. Reach out if you would like to know more.</p>
+        <div className={styles.contactBtns}>
+          <a className={styles.contactBtn} href="#contact">Let&apos;s talk</a>
+          <a className={styles.contactBtn} href="">Download CV</a>
+        </div>
       </div>
       <img className={styles.heroImg} src={getImageURL("hero/heroImage.png")} alt="hero image" />
       {/* hero image from pngtree.com */} 
