0ad2197d1b141428c37e3968b02f59467067eb75
Mariana Ramos <mary.programms@gmail.com>
2024-01-02 15:42:22 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
 
 
+
 function App() {
 
   return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import styles from './hero.module.css';
 import { getImageURL } from "../../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDownload, faComments } from "@fortawesome/free-solid-svg-icons"
 
 
 export const Hero = () => {
@@ -7,10 +9,11 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I&apos;m Mariana</h1>
-        <p className={styles.description}>I&apos;m a Frontend Developer and I have been creating <strong className={styles.span}>lading pages</strong> and <strong className={styles.span}>gamified HTML5</strong> solutions. I work with JavaScript and jQuery. Reach out if you would like to know more.</p>
+        <p className={styles.description}>I&apos;m a Frontend Developer and I create <strong className={styles.span}>Landing Pages</strong> and <strong className={styles.span}>Gamified HTML</strong> solutions. I work with React, JavaScript and jQuery, but I also</p>
+        <p className={styles.description}>Reach out if you would like to know more or work with me!</p>
         <div className={styles.contactBtns}>
-          <a className={styles.contactBtn} href="#contact">Let&apos;s talk</a>
-          <a className={styles.contactBtn} href="">Download CV</a>
+          <a className={styles.contactBtn} href="#contact">Let&apos;s talk <FontAwesomeIcon icon={faComments} /></a>
+          <a className={styles.contactBtn} href="../../assets/cv-marianaramos.pdf" download>Download CV <FontAwesomeIcon icon={faDownload} /></a>
         </div>
       </div>
       <img className={styles.heroImg} src={getImageURL("hero/heroImage.png")} alt="hero image" />
